dd0c822b899a7ace749efcdd760449ec94d45390
Katlego Magabane <katlegokay01@gmail.com>
2021-02-19 10:53:07 +0200

    Login-Registration Component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store';
+import { authInfoSuccess } from './ducks/auth.duck';
+import firebase from './firebase-config';
+import './index.css';
+import routeConfiguration from './routeConfiguration';
+import Routes from './Routes';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = configureStore({}, firebase);
 
+firebase.auth().onAuthStateChanged(user => {
+  store.dispatch(authInfoSuccess(user));
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes routes={routeConfiguration()}/>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
+
+serviceWorker.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
